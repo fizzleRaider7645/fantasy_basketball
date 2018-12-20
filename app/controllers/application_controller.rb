ba05fd72eac57da2c411b8b518ce5c3884f8a286
Douglas Uretsky <douglasuretsky@gmail.com>
2018-12-20 14:28:47 -0500

    create user, team, and player models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "carcollection"
+    set :session_secret, "basketballjones"
   end
 
   get '/' do
