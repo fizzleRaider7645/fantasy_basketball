777a3765b82eb49d183d12b00271ab3da68c9bd3
Douglas Uretsky <douglasuretsky@gmail.com>
2018-12-21 09:30:41 -0500

    create new controller for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,24 @@ class UsersController < ApplicationController
     erb :'users/login'
   end
 
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    session[:user_id] = @user.id
+    erb :'users/show'
+  end
+
+  get '/new' do
+    erb :'users/new'
+  end
+
+  post '/new' do
+    if User.exists?(:email => params[:email]) || User.exists?(:username => params[:username])
+      redirect :'/login'
+    else
+      @user = User.create(params) unless User.exists?(:email => params[:email])
+      session[:user_id] = @user.id
+      redirect :'users/show'
+    end
+  end
+
 end
