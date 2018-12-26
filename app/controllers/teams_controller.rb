fb91c78481462262d4437db58cc4586dfe16e8f6
Douglas Uretsky <douglasuretsky@gmail.com>
2018-12-26 12:39:14 -0500

    finish delete function when checkbox is unchecked

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -49,7 +49,46 @@ class TeamsController < ApplicationController
 
   patch '/teams/:id/edit' do
     if logged_in?
-      binding.pry
+      #team name check
+      if params[:team_name] != ""
+        @team = current_user.team
+        @team.update(name: params[:team_name])
+      end
+      #end of team name check
+
+      #custom player check
+      if params[:custom_player_name] != ""
+        @player = Player.create(name: params[:custom_player_name])
+        current_user.team.players << @player
+      end
+      #end of custom player check
+
+      #check for players to add from checkbox
+      if params.keys.include?("player_ids")
+        params[:player_ids].each do |id|
+          @player = Player.find(id)
+          @player.team_id = current_user.team.id
+          @player.save
+
+          #check below for diff between incoming params and current player ids
+          #and delete those players not in incoming params
+          current_ids = current_user.team.players.map { |player| player.id }
+          incoming_ids = params[:player_ids].map(&:to_i)
+          if current_ids != incoming_ids
+            hold = incoming_ids + current_ids
+            players_to_delete = hold - (incoming_ids & current_ids)
+            players_to_delete.each do |id|
+              @player = Player.find(id)
+              @player.team_id = nil
+              @player.save
+            end
+          end
+        end
+      else
+        current_user.team.players.clear
+      end
+      #end of checkbox check
     end
+    redirect :'/show'
   end
 end
