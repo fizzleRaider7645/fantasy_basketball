18402c82fa597ba71d923f116cbaf730d3adf795
Douglas Uretsky <douglasuretsky@gmail.com>
2018-12-27 11:45:36 -0500

    create delete action in teams_controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -51,12 +51,12 @@ class TeamsController < ApplicationController
 
   patch '/teams/:id/edit' do
     if logged_in?
-      #team name check
+      #team name edit check
       if params[:team_name] != ""
         @team = current_user.team
         @team.update(name: params[:team_name])
       end
-      #end of team name check
+      #end of team name edit check
 
       #custom player check
       if params[:custom_player_name] != "" && !current_user.team.at_limit?
@@ -75,25 +75,7 @@ class TeamsController < ApplicationController
       end
         #end of no params logic
 
-      #When players are checked and
-      #there are no players currently on the roster
-
-      # if params[:player_ids] && current_user.team.players.empty?
-      #   params[:player_ids].each do |id|
-      #     @player = Player.find(id)
-      #     @player.team_id = current_user.team.id
-      #     @player.save
-      #     # current_user.team.roster_spots -= 1
-      #     # current_user.team.save
-      #   end
-      #   # Clear the params hash of the :player_ids' key to avoid falling into
-      #   #other if statments
-      #   params.except!(:player_ids)
-      # end
-      #end of case when they are checked players and empty current roster
-
-      #When there are checked players and players currently on roster
-      # if params[:player_ids] && !current_user.team.players.empty?
+      #When there are checked players
       if params[:player_ids]
         current_ids = current_user.team.players.map { |player| player.id }
         incoming_ids = params[:player_ids].map(&:to_i)
@@ -107,18 +89,14 @@ class TeamsController < ApplicationController
             @player = Player.find(id)
             @player.team_id = nil
             @player.save
-            # current_user.team.roster_spots += 1
-            # current_user.team.save
           end
         end
 
-        #iterating over incoming_ids as these players should be on roster
+        # iterating over incoming_ids as these players should be on roster
         incoming_ids.each do |id|
           @player = Player.find(id)
           @player.team_id = current_user.team.id
           @player.save
-          # current_user.team.roster_spots -= 1
-          # current_user.team.save
         end
       end
       #end of check when there are checked players and players currently on roster
@@ -127,5 +105,16 @@ class TeamsController < ApplicationController
     end
     redirect :'/show'
     #**************CHECKBOX LOGIC END********************************************
+  end
+
+  delete '/teams/:id/delete' do
+    if logged_in?
+      @team = Team.find(params[:id])
+      @team.players.clear
+      Team.delete(params[:id])
+      redirect :'/show'
+    else
+      redirect :'/login'
+    end
   end
 end
